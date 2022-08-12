35e6e9eb5ac8cce5b3a5a626023517628e26ea9f
MrAA007 <youzhou011@gmail.com>
2022-08-12 22:07:55 +0800

    modify validation

--- mcp/consensus/validation.cpp
+++ mcp/consensus/validation.cpp
@@ -223,14 +223,18 @@ mcp::validate_result mcp::validation::dag_validate(mcp::db::db_transaction & tra
 				continue;
 			}
 		}
-		//std::shared_ptr<mcp::account_state> as(cache_a->latest_account_state_get(transaction_a, t->sender()));
-		//if (as && as->nonce() > t->nonce()) /// must bigger than last account state nonce
-		//{
-		//	result.code = mcp::validate_result_codes::parents_and_previous_include_invalid_block;
-		//	result.err_msg = boost::str(boost::format("Invalid missing link, hash: %1% ,nonce req: %2%, got: %3%") % link.hex() % t->nonce() % as->nonce());
-		//	return result;
-		//}
-		if (previousFrom == t->sender() && t->nonce() != accNonce+1) /// must sort and sequential growth.
+		if (previousFrom != t->sender()) ///first account's transaction in the block
+		{
+			u256 pNonce = 0;
+			auto exist = cache_a->account_nonce_get(transaction_a, t->sender(), pNonce);
+			if (exist && t->nonce() > pNonce)
+			{
+				result.code = mcp::validate_result_codes::parents_and_previous_include_invalid_block;
+				result.err_msg = boost::str(boost::format("Invalid missing link, hash: %1% ,nonce req: %2%, got: %3%") % link.hex() % t->nonce() % pNonce);
+				return result;
+			}
+		}
+		else if(t->nonce() != accNonce + 1)/// must sort and sequential growth.
 		{
 			result.code = mcp::validate_result_codes::parents_and_previous_include_invalid_block;
 			result.err_msg = boost::str(boost::format("Invalid missing link, hash: %1% ,nonce req: %2%, got: %3%") % link.hex() % t->nonce());
