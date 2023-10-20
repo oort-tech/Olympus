ae0ba2fa6034a148a7b55ca415a76554acc47e41
Paul <web3samuraidev@gmail.com>
2023-10-20 17:36:36 +0000

    some-fixes

--- test/contracts/mcp-upgradable-test/scripts/3_dwdn.js
+++ test/contracts/mcp-upgradable-test/scripts/3_dwdn.js
@@ -3,12 +3,10 @@ const { ethers } = require("hardhat")
 async function main() {
   const [owner, operator] = await ethers.getSigners()
 
-  //const DWDNContract = await ethers.getContractFactory("DWDN")
-  //const dwdnContract = await DWDNContract.deploy()
-  //await dwdnContract.deployed()
-  //console.log("> OK: DWDNContract deployed to:", dwdnContract.address)
-
-  const dwdnContract = await ethers.getContractAt('DWDN', '0x90AFD1C4Fb63d497596EAF467424Bf8724Bc36D3')
+  const DWDNContract = await ethers.getContractFactory("DWDN")
+  const dwdnContract = await DWDNContract.deploy()
+  await dwdnContract.deployed()
+  console.log("> OK: DWDNContract deployed to:", dwdnContract.address)
 
   console.log("getAddresses:", await dwdnContract.getAddresses())
 
