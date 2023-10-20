fb5b3f4fdfb191b9df53ff14e81b31d363946c28
Paul <web3samuraidev@gmail.com>
2023-10-20 08:58:27 +0000

    remove other networks

--- test/contracts/mcp-uniswapv2-core/hardhat.config.js
+++ test/contracts/mcp-uniswapv2-core/hardhat.config.js
@@ -15,18 +15,6 @@ module.exports = {
     },
   },
   networks: {
-    mainnet: {
-      url: process.env.MAINNET_END_POINT,
-      accounts: [process.env.MAINNET_PRIVATE_KEY]
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_END_POINT,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY]
-    },
-    bsc_test: {
-      url: process.env.BSC_TEST_END_POINT,
-      accounts: [process.env.BSC_TEST_PRIVATE_KEY, process.env.BSC_OTHER_KEY]
-    },
     ccnbeta: {
       chainid: 9700,
       url: process.env.CCNBETA_END_POINT,
