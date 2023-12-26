93b056e7be89ecc2d5a315a4170ac2da55160206
Paul <41552663+molecula451@users.noreply.github.com>
2023-12-26 17:15:27 +0000

    add updates

--- test/contracts/mcp-multisig/README.md
+++ test/contracts/mcp-multisig/README.md
@@ -1,3 +1,32 @@
 # MultiSig Wallet on CCNBETA
 
 ![Horizontal Logo (TM)](https://github.com/oort-tech/Olympus/assets/41552663/bc195389-b1ec-4d96-9f23-bb90415b1e36)
+
+### Project Setup and Testing Guide
+
+This repository contains the source code for the mcp-dydx project. Follow the steps below to set up the project, compile the code, and run tests.
+
+### Requirements
+- Node.js version 20 or later (20 is recommended)
+
+### Installation
+1. Navigate to the source directory in your terminal.
+2. Run the following command to install dependencies using npm:
+
+    ```bash
+    npm i --save-dev
+    ```
+3. Copy .env.example to .env and fill required values
+
+### Compilation & Deployment
+Compile the project using the following command:
+
+```bash
+npm run test_ccnbeta
+```
+
+#### Alternative
+
+You may run the provided shell script
+```bash
+./test_ccnbeta.sh # Deploy Factory and Test Protocol to CCNBETA
--- test/contracts/mcp-multisig/scripts/test_factory.js
+++ test/contracts/mcp-multisig/scripts/test_factory.js
@@ -2,7 +2,7 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 const ABI = require('../artifacts/contracts/Factory.sol/Factory.json')
 
-describe("Test MultiSig", async() => {
+describe("Test MultiSig Factory & Wallet Creation", async() => {
     const [owner, outsideUser] = await ethers.getSigners();
     let multiSigFactory;
     let deployedWalletSig;
@@ -27,19 +27,18 @@ describe("Test MultiSig", async() => {
     });
 
     it("should just create new wallet from factory multi sig using CREATE", async() => {
-        const result = await deployedWalletSig.create(["0xD2a8fd999bF4CC30A3fD86f017DeD1c6BF67E78B", "0x6080184Da41682D75562fcae090dD5eB4Fc2E771"], 2, 3000)
+        const result = await deployedWalletSig.create([owner.address, outsideUser.address], 2, 3000)
         const data = result.data;
         expect(data).to.be.equal(result.data);
     })
     it("Should return correct number of contracts instantiations", async() => {
-        const [owner, outsideUser] = await ethers.getSigners();
         const result = await deployedWalletSig.connect(owner).create([owner.address, outsideUser.address], 2, 3000)
         const contract = new ethers.Contract(result.to, ABI.abi, owner);
         const count = (await contract.getInstantiationCount(owner.address));
-        expect(count).to.be.equal(1);
+        expect(count).to.be.equal(2);
     });
     it("Should create wallets MultiSig from factory and return correct MultiSig Address", async() => {
-        const result = await deployedWalletSig.connect(owner).create(["0xD2a8fd999bF4CC30A3fD86f017DeD1c6BF67E78B", "0x6080184Da41682D75562fcae090dD5eB4Fc2E771"], 2, 3000);
+        const result = await deployedWalletSig.connect(owner).create([owner.address, outsideUser.address], 2, 3000);
         const contract = new ethers.Contract(result.to, ABI.abi, owner);
         console.log(await contract.instantiations(owner.address,0), "MultiSig Address");
         })
